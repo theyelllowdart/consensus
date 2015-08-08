1c0f8e704785137033727cf17c64513aea2d6b9c
aaron <theyelllowdart@gmail.com>
2015-08-08 16:42:09 -0400

    support multiple rooms

--- server/generated/rooms.js
+++ server/generated/rooms.js
@@ -1,8 +1,17 @@
 var _ = require('lodash');
 function setup(io, db) {
-    var songQueue = [];
+    var rooms = {};
     var delaySongDuration = 1000;
     io.on('connection', function (socket) {
+        var room = socket.request._query.room;
+        if (!rooms[room]) {
+            rooms[room] = [];
+        }
+        var songQueue = rooms[room];
+        socket.join(room);
+        function getRoomSocket() {
+            return io.sockets.to(room);
+        }
         var userEmail = socket.request.user;
         console.log('user ' + userEmail + ' connected');
         socket.on('disconnect', function () {
@@ -14,7 +23,7 @@ function setup(io, db) {
         });
         socket.on('clear', function () {
             songQueue = [];
-            io.emit('queueChange', songQueue);
+            getRoomSocket().emit('queueChange', songQueue);
         });
         socket.on('enqueue', function (song) {
             song.creator = userEmail;
@@ -26,7 +35,7 @@ function setup(io, db) {
                 startSong(io, songQueue[0]);
             }
             db.then(function (x) { return x.query('INSERT into Song (id, creator, url, duration, source, name, upvotes, downvotes, start, scheduled) ' + 'VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)', [song.id, song.creator, song.url, song.duration, song.source, song.name, song.upvotes, song.downvotes, song.start, song.scheduled]); });
-            io.emit('queueChange', songQueue);
+            getRoomSocket().emit('queueChange', songQueue);
         });
         socket.on('upvote', function (id) {
             vote(io, id, userEmail, function (song) {
@@ -43,48 +52,48 @@ function setup(io, db) {
             });
         });
         socket.on('status', function () {
-            socket.emit('queueChange', songQueue);
+            getRoomSocket().emit('queueChange', songQueue);
         });
-    });
-    function vote(io, id, userEmail, sameVotesFn, oppositeVotesFn) {
-        var index = _.findIndex(songQueue, function (song) {
-            return song.id === id;
-        });
-        if (index === -1)
-            return false;
-        var song = songQueue[index];
-        _.remove(oppositeVotesFn(song), function (email) {
-            return email === userEmail;
-        });
-        var nullifiedVote = _.remove(sameVotesFn(song), function (email) {
-            return email === userEmail;
-        });
-        if (nullifiedVote.length === 0) {
-            sameVotesFn(song).push(userEmail);
-        }
-        if (song.downvotes.length >= song.upvotes.length) {
-            songQueue.splice(index, 1);
-            if (index === 0 && songQueue.length > 0) {
-                startSong(io, songQueue[0]);
+        function vote(io, id, userEmail, sameVotesFn, oppositeVotesFn) {
+            var index = _.findIndex(songQueue, function (song) {
+                return song.id === id;
+            });
+            if (index === -1)
+                return false;
+            var song = songQueue[index];
+            _.remove(oppositeVotesFn(song), function (email) {
+                return email === userEmail;
+            });
+            var nullifiedVote = _.remove(sameVotesFn(song), function (email) {
+                return email === userEmail;
+            });
+            if (nullifiedVote.length === 0) {
+                sameVotesFn(song).push(userEmail);
             }
-        }
-        io.emit('queueChange', songQueue);
-    }
-    function startSong(io, song) {
-        function endSongTimeoutFn(io, song) {
-            setTimeout(function () {
-                // guard for case if downvoting removes song
-                if (songQueue.length > 0 && songQueue[0].id === song.id) {
-                    songQueue.shift();
-                    if (songQueue.length > 0) {
-                        startSong(io, songQueue[0]);
-                    }
-                    io.emit('queueChange', songQueue);
+            if (song.downvotes.length >= song.upvotes.length) {
+                songQueue.splice(index, 1);
+                if (index === 0 && songQueue.length > 0) {
+                    startSong(io, songQueue[0]);
                 }
-            }, song.duration + (delaySongDuration * 2));
+            }
+            getRoomSocket().emit('queueChange', songQueue);
         }
-        song.start = Date.now() + delaySongDuration;
-        endSongTimeoutFn(io, song);
-    }
+        function startSong(io, song) {
+            function endSongTimeoutFn(io, song) {
+                setTimeout(function () {
+                    // guard for case if downvoting removes song
+                    if (songQueue.length > 0 && songQueue[0].id === song.id) {
+                        songQueue.shift();
+                        if (songQueue.length > 0) {
+                            startSong(io, songQueue[0]);
+                        }
+                        getRoomSocket().emit('queueChange', songQueue);
+                    }
+                }, song.duration + (delaySongDuration * 2));
+            }
+            song.start = Date.now() + delaySongDuration;
+            endSongTimeoutFn(io, song);
+        }
+    });
 }
 exports.setup = setup;
--- server/rooms.ts
+++ server/rooms.ts
@@ -1,13 +1,19 @@
 var _ = require('lodash');
 
 export function setup(io:SocketIO.Server, db:any) {
-  var songQueue = [];
+  var rooms = {};
   var delaySongDuration = 1000;
 
   io.on('connection', function (socket) {
-    var room = socket.request.query.room;
+    var room = socket.request._query.room;
+    if (!rooms[room]) {
+      rooms[room] = [];
+    }
+    var songQueue = rooms[room];
     socket.join(room);
-    var roomSocket = socket.to(room);
+    function getRoomSocket() {
+      return io.sockets.to(room);
+    }
 
     var userEmail = socket.request.user;
     console.log('user ' + userEmail + ' connected');
@@ -20,7 +26,7 @@ export function setup(io:SocketIO.Server, db:any) {
     });
     socket.on('clear', function () {
       songQueue = [];
-      roomSocket.emit('queueChange', songQueue);
+      getRoomSocket().emit('queueChange', songQueue);
     });
     socket.on('enqueue', function (song) {
       song.creator = userEmail;
@@ -36,7 +42,7 @@ export function setup(io:SocketIO.Server, db:any) {
         'VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)',
         [song.id, song.creator, song.url, song.duration, song.source, song.name, song.upvotes, song.downvotes, song.start, song.scheduled]
       ));
-      io.emit('queueChange', songQueue);
+      getRoomSocket().emit('queueChange', songQueue);
     });
     socket.on('upvote', function (id) {
       vote(io, id, userEmail, function (song) {
@@ -53,49 +59,49 @@ export function setup(io:SocketIO.Server, db:any) {
       })
     });
     socket.on('status', function () {
-      socket.emit('queueChange', songQueue);
+      getRoomSocket().emit('queueChange', songQueue);
     });
-  });
 
-  function vote(io, id, userEmail, sameVotesFn, oppositeVotesFn) {
-    var index = _.findIndex(songQueue, function (song) {
-      return song.id === id;
-    });
-    if (index === -1) return false;
-    var song = songQueue[index];
-    _.remove(oppositeVotesFn(song), function (email) {
-      return email === userEmail;
-    });
-    var nullifiedVote = _.remove(sameVotesFn(song), function (email) {
-      return email === userEmail;
-    });
-    if (nullifiedVote.length === 0) {
-      sameVotesFn(song).push(userEmail)
-    }
-    if (song.downvotes.length >= song.upvotes.length) {
-      songQueue.splice(index, 1);
-      if (index === 0 && songQueue.length > 0) {
-        startSong(io, songQueue[0]);
+    function vote(io, id, userEmail, sameVotesFn, oppositeVotesFn) {
+      var index = _.findIndex(songQueue, function (song) {
+        return song.id === id;
+      });
+      if (index === -1) return false;
+      var song = songQueue[index];
+      _.remove(oppositeVotesFn(song), function (email) {
+        return email === userEmail;
+      });
+      var nullifiedVote = _.remove(sameVotesFn(song), function (email) {
+        return email === userEmail;
+      });
+      if (nullifiedVote.length === 0) {
+        sameVotesFn(song).push(userEmail)
       }
-    }
-    io.emit('queueChange', songQueue);
-  }
-
-  function startSong(io, song) {
-    function endSongTimeoutFn(io, song) {
-      setTimeout(function () {
-        // guard for case if downvoting removes song
-        if (songQueue.length > 0 && songQueue[0].id === song.id) {
-          songQueue.shift();
-          if (songQueue.length > 0) {
-            startSong(io, songQueue[0])
-          }
-          io.emit('queueChange', songQueue);
+      if (song.downvotes.length >= song.upvotes.length) {
+        songQueue.splice(index, 1);
+        if (index === 0 && songQueue.length > 0) {
+          startSong(io, songQueue[0]);
         }
-      }, song.duration + (delaySongDuration * 2));
+      }
+      getRoomSocket().emit('queueChange', songQueue);
     }
 
-    song.start = Date.now() + delaySongDuration;
-    endSongTimeoutFn(io, song);
-  }
+    function startSong(io, song) {
+      function endSongTimeoutFn(io, song) {
+        setTimeout(function () {
+          // guard for case if downvoting removes song
+          if (songQueue.length > 0 && songQueue[0].id === song.id) {
+            songQueue.shift();
+            if (songQueue.length > 0) {
+              startSong(io, songQueue[0])
+            }
+            getRoomSocket().emit('queueChange', songQueue);
+          }
+        }, song.duration + (delaySongDuration * 2));
+      }
+
+      song.start = Date.now() + delaySongDuration;
+      endSongTimeoutFn(io, song);
+    }
+  });
 }
